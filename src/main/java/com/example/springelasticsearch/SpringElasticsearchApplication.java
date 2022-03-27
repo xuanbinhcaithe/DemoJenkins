9a34624ddc00558fa11655424047d7c7dc297792
xuanbinhcaithe <nguyenxuanbinh1205@gmail.com>
2022-03-27 17:20:13 +0700

    chang file pom

--- src/main/java/com/example/springelasticsearch/SpringElasticsearchApplication.java
+++ src/main/java/com/example/springelasticsearch/SpringElasticsearchApplication.java
@@ -2,12 +2,10 @@ package com.example.springelasticsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableElasticsearchRepositories("com.example.springelasticsearch.repo.elastic")
 public class SpringElasticsearchApplication {
 
     public static void main(String[] args) {
