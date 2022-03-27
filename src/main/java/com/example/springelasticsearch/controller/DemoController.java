66621a3a31100b8e80a9be387a29a507f183b8c2
xuanbinhcaithe <nguyenxuanbinh1205@gmail.com>
2022-03-27 17:01:11 +0700

    add war in application.properties

--- src/main/java/com/example/springelasticsearch/controller/DemoController.java
+++ src/main/java/com/example/springelasticsearch/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.example.springelasticsearch.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ public class DemoController {
     @GetMapping("/say")
     public String sayHello() {
         return "Hello Xuan Binh";
+    }
+
+    @GetMapping("/sayHello/{name}")
+    public String sayName(@PathVariable("name") String name) {
+        return "Hello" + name;
     }
 }
